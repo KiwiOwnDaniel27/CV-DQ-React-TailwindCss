932d25d5a8b40335be3f0300cee9fd39750f2f54
Daniel Quiguango <fiel2589@hotmail.com>
2025-10-10 17:30:17 -0500

    Cambios generales web

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/cv-25-react-tailwind/', 
 })
 
