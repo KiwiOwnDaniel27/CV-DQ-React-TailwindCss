0ffd4995bc3ce6f9e642b4e0c8444931fc8a5810
Daniel Quiguango <fiel2589@hotmail.com>
2025-12-08 23:56:57 -0500

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg"
           data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
           <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md sm:hidden" loading="lazy" />
-          <p className="text-base/loose mb-10">Hola, me presento, Soy Daniel Quiguango, desarrollador web Jr especializado en diseño UI/IX
+          <p className="text-base/loose mb-10">Hola, me presento, Soy Daniel Quiguango, desarrollador web Jr especializado en diseño UX/UI
             y en creación de productos digitales. Estoy muy consiente que el diseño y la funcionalidad de una
             página web o App deben ir muy de la mano con la experiencia del Usuario, y que está sea óptima,
             para que cada proyecto desarrollado no solo sea atractivo si no que cumpla con las expectativas de
