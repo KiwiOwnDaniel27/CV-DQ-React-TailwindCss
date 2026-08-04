62fc185d4fc7f208ed68c4d660d57854d89b4ce3
Daniel Quiguango <fiel2589@hotmail.com>
2026-08-04 00:39:25 -0500

    changes finally

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ const Footer = () => {
     return (
         <div className="mt-11 py-4 flex md:flex-row flex-col gap-6 md:gap-0 justify-between items-center">
             <h1 className="text-2xl font-bold">Portafolio</h1>
-            <div className="flex gap-9">
+            <div className="flex gap-9 ml-0 sm:ml-20">
                 <a href="#inicio">Ir a Inicio</a>
-                <a href="#certificados">Certificacionses</a>
+                <a href="#certificados">Certificaciones</a>
                 <a href="#project">Proyectos</a>
             </div>
-            <div className="flex items-center gap-3 mb-27">
+            <div className="flex items-center gap-3 mb-27 md:mb-0">
                 <a href="https://github.com/KiwiOwnDaniel27" target="_blank">
                     <i className="ri-github-fill ri-2x"></i>
                 </a>
